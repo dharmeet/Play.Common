ccf95bafabd1d1302bdf621d6ab6f3748713e109
Dharmeet Singh <dharmeetkhalsa@gmail.com>
2025-09-03 17:22:58 +0530

    Created a common project for creating the nuget package for MongoDB configuration and common queries.

---
+++ src/Play.Common/IRepository.cs
@@ -0,0 +1,19 @@
+using System.Linq.Expressions;
+
+namespace Play.Common
+{
+    public interface IRepository<T> where T : IEntity
+    {
+        Task<T> GetAsync(Guid id);
+
+        Task<T> GetAsync(Expression<Func<T, bool>> filter);
+        
+        Task<IReadOnlyCollection<T>> GetAllAsync();
+
+        Task<IReadOnlyCollection<T>> GetAllAsync(Expression<Func<T, bool>> filter);
+
+        Task CreateAsync(T item);
+        Task UpdateAsync(T item);
+        Task DeleteAsync(Guid id);
+    }
+}
